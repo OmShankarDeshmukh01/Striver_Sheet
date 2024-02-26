ac42dd597b8be373ed6d543e410c7c3554e6e55f
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2024-02-26 17:46:18 +0530

    added

---
+++ SOLVE_PROBLEMS_ON_ARRAYS/MEDIUM/Sort_Colors_75.java
@@ -0,0 +1,15 @@
+package SOLVE_PROBLEMS_ON_ARRAYS.MEDIUM;
+
+public class Sort_Colors_75 {
+    public void sortColors(int[] nums) {
+        for(int i =0 ;i<nums.length-1 ;i++){
+            for(int j =i+1 ;j<nums.length; j++){
+                if(nums[i] > nums[j]){
+                   int temp = nums[i];
+                   nums[i] = nums[j];
+                   nums[j] = temp ;
+                }
+            }
+        }
+    }
+}
