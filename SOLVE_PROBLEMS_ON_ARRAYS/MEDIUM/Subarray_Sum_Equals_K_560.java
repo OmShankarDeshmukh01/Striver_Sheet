d0b3bcda3efe5286dbfc1b8e5d3696f08a440e34
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2024-02-27 20:36:27 +0530

    added

---
+++ SOLVE_PROBLEMS_ON_ARRAYS/MEDIUM/Subarray_Sum_Equals_K_560.java
@@ -0,0 +1,21 @@
+public class Subarray_Sum_Equals_K_560 {
+    //bad approach
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length; // size of the given array.
+        int count = 0; // Number of subarrays:
+
+        for (int i = 0 ; i < n; i++) { // starting index i
+            int sum = 0;
+            for (int j = i; j < n; j++) { // ending index j
+                // calculate the sum of subarray [i...j]
+                // sum of [i..j-1] + arr[j]
+                sum += nums[j];
+
+                // Increase the count if sum == k:
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
