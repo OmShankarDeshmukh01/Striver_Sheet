1a19b7e03053f83fc52d62383235c16385425bd4
OmShankarDeshmukh01 <omdeshmukhstudy@gmail.com>
2024-03-04 21:50:05 +0530

    added

--- BINARY_SEARCH/Binary_Search_on_1D_Arrays/Search_Insert_Position.java
+++ BINARY_SEARCH/Binary_Search_on_1D_Arrays/Search_Insert_Position.java
@@ -1,16 +1,17 @@
 package BINARY_SEARCH.Binary_Search_on_1D_Arrays;
-// arr[index] >= x
-public class Implement_Lower_Bound {
-    public static int lowerBound(int []arr, int x) {
+//aply lowerbound  in this question
+public class Search_Insert_Position {
+    public static int lowerBound(int []arr, int target) {
         int n = arr.length;
         int start = 0;
-        int end = n -1;
+        int end = n-1;
         int ans = n;
-        while(start <= end){
-            int mid = (start + end)/2;
-            if( arr[mid] >= x){
-                ans = mid;
-                end = mid -1;
+        while( start <= end){
+            int mid = (start+end)/2;
+
+            if(arr[mid] >= target){
+                ans =mid;
+                end =mid-1;
             }
             else{
                 start = mid+1;
